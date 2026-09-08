d796b66e3128c351ebdbd3a81ee514a490552afa
Walter Cervini <wcervini@gmail.com>
2026-09-08 10:54:51 +0100

    Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,2 @@
-const unused_var = "Walter";
-let url = "https://www.tecnobyte.es";
-
-url = "esto es una prueba";
-console.log(url);
-console.log(unused_var);
+const mode = import.meta.env.MODE === "production" ? "production" : "development";
+console.log(mode);
